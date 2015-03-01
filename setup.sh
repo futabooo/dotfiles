7e6ebd67ab613dd75aa0024c4a9797000e7029e6
futabooo <mail@futabooo.com>
2015-03-02 01:47:07 +0900

    Add .zshrc from oh-mh-zsh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-DOT_FILES=( .vim .vimrc .gitconfig .bash_profile .bashrc)
+# install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# set oh-my-zsh theme
+THEME=solarized-powerline.zsh-theme
+ln -s $HOME/dotfiles/.zsh/oh-my-zsh-solarized-powerline-theme/$THEME $HOME/.oh-my-zsh/themes
+
+# create dotfiles symbolic link
+DOT_FILES=( .vim .vimrc .gitconfig .bash_profile .bashrc .zshrc)
 
 for file in ${DOT_FILES[@]}
 do
@@ -11,10 +19,6 @@ do
 	echo "シンボリックリンクをはりました: $file"
   fi
 done
-
-# oh-my-zshのテーマを設定
-THEME=solarized-powerline.zsh-theme
-ln -s $HOME/dotfiles/oh-my-zsh-solarized-powerline-theme/$THEME $HOME/.oh-my-zsh/themes
 
 # ターミナルの不可視項目を表示
 defaults write com.apple.finder AppleShowAllFiles -boolean true
